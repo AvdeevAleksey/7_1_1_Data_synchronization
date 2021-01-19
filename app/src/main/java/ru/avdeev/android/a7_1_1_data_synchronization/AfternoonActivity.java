c50af09aa82a412e2013af053fcb07d2549cef88
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2021-01-19 16:49:28 +0300

    Initial commit

---
+++ app/src/main/java/ru/avdeev/android/a7_1_1_data_synchronization/AfternoonActivity.java
@@ -0,0 +1,14 @@
+package ru.avdeev.android.a7_1_1_data_synchronization;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AfternoonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_afternoon);
+    }
+}
